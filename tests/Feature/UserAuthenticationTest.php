47a044cf6d4b2cd62a6987b56f14ec0872aa211d
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-11-15 18:04:48 +0700

    feat: cannot login with wrong credentials

--- tests/Feature/UserAuthenticationTest.php
+++ tests/Feature/UserAuthenticationTest.php
@@ -8,16 +8,16 @@ use Illuminate\Support\Facades\Artisan;
 
 class UserAuthenticationTest extends TestCase
 {
+    private $user;
+
     public function test_user_can_login_with_correct_credentials(): void
     {
         $this->setupDatabase();
-        $phone = '081265789189';
-        $password = '12345678';
-        User::factory()->create(['phone' => $phone, 'password' => $password]);
+        $this->createUser();
 
         $response = $this->postJson('/api/login', [
-            'phone' => $phone,
-            'password' => $password
+            'phone' => $this->user->phone,
+            'password' => $this->user->passwordPlain
         ]);
 
         $response->assertOk();
@@ -26,8 +26,30 @@ class UserAuthenticationTest extends TestCase
         ]);
     }
 
+    public function test_user_cannot_login_with_incorrect_credentials(): void
+    {
+        $this->setupDatabase();
+        $this->createUser();
+        $otherPhone = '081335356767';
+        
+        $response = $this->postJson('/api/login', [
+            'phone' => $otherPhone,
+            'password' => $this->user->password
+        ]);
+
+        $response->assertUnauthorized();
+    }
+
     private function setupDatabase()
     {
         Artisan::call('migrate');
+    }
+
+    private function createUser()
+    {
+        $phone = '081265789189';
+        $password = '12345678';
+        $this->user = User::factory()->create(['phone' => $phone, 'password' => $password]);
+        $this->user->passwordPlain = $password;
     }
 }
