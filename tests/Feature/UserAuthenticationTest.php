19f035e935399d665a86562316e30c1fa34e759c
Ikhwanu Robik Saputra <robikikhwan@gmail.com>
2025-11-15 17:26:32 +0700

    refactor: removed unnecessary test

--- tests/Feature/UserAuthenticationTest.php
+++ tests/Feature/UserAuthenticationTest.php
@@ -2,22 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserAuthenticationTest extends TestCase
 {
-   public function test_user_has_api_tokens(): void
     {
-        Artisan::call("migrate");
-        $user = User::factory()->create();
-        $user->createToken("my token");
 
-        $tokens = $user->tokens;
 
-        $this->assertNotCount(0, $tokens);
     }
 }
